33bc289674b09d2c0c7e6176dc8c7e0156e9da3b
Jordan Hilado <jordanalihilado@gmail.com>
2023-10-16 13:29:57 -0700

    update scripts

--- examples/text_to_image/finetune-kream.sh
+++ examples/text_to_image/finetune-kream.sh
@@ -1,6 +1,6 @@
-export MODEL_NAME="CompVis/stable-diffusion-v1-1"
-export OUTPUT_DIR="sd-1-1-kream-lora"
-export HUB_MODEL_ID="jordanhilado/sd-1-1-kream-lora"
+export MODEL_NAME="runwayml/stable-diffusion-v1-5"
+export OUTPUT_DIR="sd-1-5-kream-lora"
+export HUB_MODEL_ID="jordanhilado/sd-1-5-kream-lora"
 export DATASET_NAME="hahminlew/kream-product-blip-captions"
 
 accelerate launch --mixed_precision="fp16"  train_text_to_image_lora.py \
--- examples/text_to_image/inference.py
+++ examples/text_to_image/inference.py
@@ -6,7 +6,9 @@ lora_model_id = "jordanhilado/sd-1-5-pokemon-lora"
 card = RepoCard.load(lora_model_id)
 base_model_id = card.data.to_dict()["base_model"]
 
-pipe = StableDiffusionPipeline.from_pretrained(base_model_id, torch_dtype=torch.float16, use_safetensors=True, safety_checker=None)
+pipe = StableDiffusionPipeline.from_pretrained(
+    base_model_id, torch_dtype=torch.float16, use_safetensors=True, safety_checker=None
+)
 pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
 
 pipe.unet.load_attn_procs(lora_model_id)
@@ -24,20 +26,22 @@ pipe.nsfw_filter = lambda x: False
 
 prompt = "A pokemon red and white cartoon ball with an angry look on its face"
 
+
 def filename(prompt, scale, iteration):
     return f"pokemon_red_white_ball_scale_{scale}_{iteration}.png"
+
 
 # generate four images using only base model weights
 for i in range(4):
     image = pipe(prompt, num_inference_steps=25, guidance_scale=7.5, cross_attention_kwargs={"scale": 0}).images[0]
-    image.save(filename(prompt, 0, i))
+    image.save("generated_images/" + filename(prompt, 0, i))
 
 # generate four images using 0.5x lora model weights
 for i in range(4):
     image = pipe(prompt, num_inference_steps=25, guidance_scale=7.5, cross_attention_kwargs={"scale": 0.5}).images[0]
-    image.save(filename(prompt, 0.5, i))
+    image.save("generated_images/" + filename(prompt, 0.5, i))
 
 # generate four images using only the lora model weights
 for i in range(4):
     image = pipe(prompt, num_inference_steps=25, guidance_scale=7.5, cross_attention_kwargs={"scale": 1}).images[0]
-    image.save(filename(prompt, 1, i))
+    image.save("generated_images/" + filename(prompt, 1, i))
