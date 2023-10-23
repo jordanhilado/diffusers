fc392cc2cdb2286c8873f924723f5f282dc29bed
Jordan Ali Hilado <jordanalihilado@gmail.com>
2023-10-23 11:16:41 -0700

    update scripts

--- examples/text_to_image/finetune-sketch.sh
+++ examples/text_to_image/finetune-sketch.sh
@@ -1,6 +1,6 @@
-export MODEL_NAME="CompVis/stable-diffusion-v1-1"
-export OUTPUT_DIR="sd-1-1-sketch-scene"
-export HUB_MODEL_ID="jordanhilado/sd-1-1-sketch-scene"
+export MODEL_NAME="runwayml/stable-diffusion-v1-5"
+export OUTPUT_DIR="sd-1-5-sketch-lora"
+export HUB_MODEL_ID="jordanhilado/sd-1-5-sketch-lora"
 export DATASET_NAME="zoheb/sketch-scene"
 
 accelerate launch --mixed_precision="fp16"  train_text_to_image_lora.py \
