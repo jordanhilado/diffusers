5970e85cbc2b8375642b9a02a3affa2c9c9fd9c8
Jordan Hilado <jordanalihilado@gmail.com>
2023-10-23 11:48:18 -0700

    update inference

--- examples/text_to_image/inference.py
+++ examples/text_to_image/inference.py
@@ -10,16 +10,13 @@ models = [
     "jordanhilado/sd-1-1-kream-lora",
 ]
 
-lora_model_id = models[0]
+lora_model_id = models[1]
 
 prompt = "A pokemon red and white cartoon ball with an angry look on its face"
 
 
-def filename(prompt, scale):
+def filename(scale):
     return f"pokemon_red_white_ball_scale_{scale}.png"
-
-
-########################################
 
 
 card = RepoCard.load(lora_model_id)
@@ -33,46 +30,22 @@ pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
 pipe.unet.load_attn_procs(lora_model_id)
 pipe.to("cuda")
 
-# image = pipe(
-#     "A pokemon with blue eyes.", num_inference_steps=25, guidance_scale=7.5, cross_attention_kwargs={"scale": 0}
-# ).images[0]
-
-# image = pipe("A pokemon with blue eyes.", num_inference_steps=25, guidance_scale=7.5).images[0]
-# image.save("blue_pokemon_scale_0.png")
-
-# turn off nsfw filter
 pipe.nsfw_filter = lambda x: False
 
-# generate four images using only base model weights
-scale_0_images = []
-for i in range(4):
-    image = pipe(prompt, num_inference_steps=25, guidance_scale=7.5, cross_attention_kwargs={"scale": 0}).images[0]
-    scale_0_images.append(image)
-    # image.save("generated_images/" + filename(prompt, 0, i))
+crop = {
+    0: (0, 0),
+    1: (512, 0),
+    2: (0, 512),
+    3: (512, 512),
+}
 
-# crop all four images in scale_0_images into a single image using Pillow
-width, height = scale_0_images[0].size
+scales = [0, 0.5, 1]
 
-# Create a new image with the dimensions for the collage
-collage_width = 2 * width  # Combine two images in each row
-collage_height = 2 * height  # Combine two images in each column
-collage = Image.new("RGB", (collage_width, collage_height))
-
-# Paste the four images onto the collage
-collage.paste(scale_0_images[0], (0, 0))
-collage.paste(scale_0_images[1], (width, 0))
-collage.paste(scale_0_images[2], (0, height))
-collage.paste(scale_0_images[3], (width, height))
-
-# Save the collage as a new image
-collage.save("generated_images/" + filename(prompt, 0))
-
-# # generate four images using 0.5x lora model weights
-# for i in range(4):
-#     image = pipe(prompt, num_inference_steps=25, guidance_scale=7.5, cross_attention_kwargs={"scale": 0.5}).images[0]
-#     image.save("generated_images/" + filename(prompt, 0.5, i))
-
-# # generate four images using only the lora model weights
-# for i in range(4):
-#     image = pipe(prompt, num_inference_steps=25, guidance_scale=7.5, cross_attention_kwargs={"scale": 1}).images[0]
-#     image.save("generated_images/" + filename(prompt, 1, i))
+for scale in scales:
+    scale_collage = Image.new("RGB", (1024, 1024))
+    for i in range(4):
+        image = pipe(
+            prompt, num_inference_steps=25, guidance_scale=7.5, cross_attention_kwargs={"scale": scale}
+        ).images[0]
+        scale_collage.paste(image, crop[i])
+    scale_collage.save("generated_images/" + filename(scale))
